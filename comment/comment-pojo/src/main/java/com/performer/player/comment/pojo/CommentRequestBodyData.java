37bfefd3d021bf7606c8859b7a3be69337452421
DESKTOP-CLCSNSU\lin1heart <406644209@qq.com>
2018-09-07 01:24:01 +0800

    return message

---
+++ comment/comment-pojo/src/main/java/com/performer/player/comment/pojo/CommentRequestBodyData.java
@@ -0,0 +1,43 @@
+package com.performer.player.comment.pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * 评论请求数据部
+ * @author lin1heart
+ *
+ */
+public class CommentRequestBodyData {
+	private static final String CONTENT = "co";
+	private static final String CREATE_TIME = "crti";
+	private static final String USERNAME = "usna";
+	private static final String REPLY_ID = "reid";
+
+	@JsonProperty(CONTENT)
+    private String content;
+
+	@JsonProperty(CREATE_TIME)
+    private String create_time;
+
+	@JsonProperty(USERNAME)
+    private String username;
+
+	@JsonProperty(REPLY_ID)
+    private String reply_id;
+
+	public String getContent() {
+		return content;
+	}
+
+	public String getCreate_time() {
+		return create_time;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getReply_id() {
+		return reply_id;
+	}
+}
--- comment/comment-web/src/main/java/com/performer/player/comment/controller/comment.java
+++ comment/comment-web/src/main/java/com/performer/player/comment/controller/comment.java
@@ -2,12 +2,20 @@ package com.performer.player.comment.controller;
 
 import com.performer.player.comment.impl.CommentImpl;
 import com.performer.player.comment.pojo.Comment;
+import com.performer.player.comment.pojo.CommentRequestBodyData;
+import com.performer.player.comment.pojo.ResultUtil;
+import com.performer.player.comment.pojo.ReturnMsg;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class comment {
@@ -20,8 +28,8 @@ public class comment {
         return com;
     }
 
-    @RequestMapping(value = "addComment",method = RequestMethod.POST)
-    public String addComment(Comment request){
+    @RequestMapping(value = "/addComment",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ReturnMsg addComment(@RequestBody CommentRequestBodyData request){
         Comment com = new Comment();
         com.setContent(request.getContent());
         com.setCreate_time(request.getCreate_time());
@@ -29,6 +37,8 @@ public class comment {
         com.setNumber_of_praise(0);
         com.setUsername(request.getUsername());
         commentImpl.insert(com);
-        return "success";
+        Map<String, String> a = new HashMap<String, String>();
+        a.put("da", "success");
+        return ResultUtil.success(a);
     }
 }
