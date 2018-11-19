365fc412b1b638576071cc545a555e4bf4dfc8df
lin1heart <lin1heart@gmail.com>
2018-11-19 23:55:47 +0800

    packing player is ok

---
+++ common/src/main/java/com/performer/player/common/utils/ResultUtil.java
@@ -0,0 +1,26 @@
+package com.performer.player.common.utils;
+
+public class ResultUtil {
+    /**
+     * 请求成功返回
+     * @param object
+     * @return
+     */
+    public static ReturnMsg success(Object object){
+    	ReturnMsg msg=new ReturnMsg();
+        msg.setCode(200);
+        msg.setMsg("请求成功");
+        msg.setData(object);
+        return msg;
+    }
+    public static ReturnMsg success(){
+        return success(null);
+    }
+
+    public static ReturnMsg error(Integer code,String resultmsg){
+    	ReturnMsg msg=new ReturnMsg();
+        msg.setCode(code);
+        msg.setMsg(resultmsg);
+        return msg;
+    }
+}
