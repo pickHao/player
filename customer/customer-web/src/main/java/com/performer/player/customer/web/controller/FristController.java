d47c4804502b5f437441031c08b20b06ed2a1954
pickHao <zzh10win@163.com>
2018-11-20 08:26:01 +0800

    添加swagger2的配置

--- customer/customer-web/src/main/java/com/performer/player/customer/web/controller/FristController.java
+++ customer/customer-web/src/main/java/com/performer/player/customer/web/controller/FristController.java
@@ -1,7 +1,10 @@
 package com.performer.player.customer.web.controller;
 
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,8 +13,15 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class FristController {
 
-    @RequestMapping("/index")
-    public String pageInfo(HttpServletRequest request) {
+    /**
+     * 用户信息查询
+     * @param userId
+     * @return
+     */
+    @ApiOperation(value = "用户信息",notes = "根据UserId查询信息")
+    @ApiImplicitParam(name = "userId", value = "用户ID", required = true, dataType = "String")
+    @RequestMapping(value = "user",method = RequestMethod.POST)
+    public String pageInfo(@RequestBody String userId) {
         return "welcome";
     }
 }
---
+++ customer/customer-web/src/main/java/com/performer/player/customer/web/util/Swagger2Config.java
@@ -0,0 +1,80 @@
+package com.performer.player.customer.web.util;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+/**
+ * @author zzh
+ * Swagger2 的配置类
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi(){
+
+        return  new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.comstar.cnp.cockpit"))
+                .paths(PathSelectors.any())
+                .build();
+
+    }
+
+//    @Bean
+//    public Docket restConfig() {
+//        return new Docket(DocumentationType.SWAGGER_2).groupName("jax-rs").apiInfo(restInfo()).forCodeGeneration(true)
+//                .pathMapping("/cockpit").select().paths(PathSelectors.any())// 过滤的接口
+//                .build().useDefaultResponseMessages(false);
+//    }
+
+
+  /*private Predicate<String> paths() {
+       return or(regex("/cockpit/.*"), regex("/rest/.*")); //
+   }*/
+
+    /**
+     * springboot jersery swagger2 的整合
+     * @return
+     */
+//    private ApiInfo restInfo() {
+//        return new ApiInfoBuilder().title("berheley service rest api ")// 大标题
+//                .description("spring boot webservice 平台 API")// 小标题
+//                .version("2.0").build();
+//    }
+
+    /**
+     * springboot springmvc的整合
+     * @return
+     */
+    private ApiInfo apiInfo(){
+        //String title, String description, String version, String termsOfServiceUrl, Contact contact, String license, String licenseUrl, Collection<VendorExtension> vendorExtensions
+        String title="用户信息接口";
+        String description="用户信息相关接口";
+        String version="1.0";
+        String termsOfServiceUrl="termsOfServiceUrl";
+
+      // String name, String url, String email
+        Contact contact=new Contact("customer","www.customer.com","mail@customer.com");
+        String license="license";
+        String licenseUrl="licenseUrl";
+        Collection<VendorExtension> vendorExtensions=new ArrayList<>();
+        return new ApiInfo(title,description,version,termsOfServiceUrl,contact,license,licenseUrl,vendorExtensions);
+    }
+}
