5501c581162adb7be80cd7afb6e29c040bdccd26
lin1heart <lin1heart@gmail.com>
2018-11-26 18:05:20 +0800

    callback

--- video/video-web/src/main/java/com/performer/player/video/controller/video.java
+++ video/video-web/src/main/java/com/performer/player/video/controller/video.java
@@ -3,13 +3,23 @@ package com.performer.player.video.controller;
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.common.utils.BinaryUtil;
 import com.aliyun.oss.model.PolicyConditions;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.google.gson.Gson;
 import com.performer.player.video.entity.callBackBodyData;
 import com.performer.player.video.entity.getOssSignResponseBodyData;
 
 import net.sf.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -18,6 +28,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -54,7 +68,7 @@ public class video {
   	@Value("${oss.callback.url}")
   	private String callbackUrl;
   	//发起回调时body的值 支持oss系统变量、自定义变量和常量
-  	private String callbackBody = "{\"bucket\":${bucket},\"mimeType\":${mimeType},\"size\":${size},\"object\":${object},\"etag\":${etag},\"imageInfo.height\":${imageInfo.height},\"imageInfo.width\":${imageInfo.width},\"imageInfo.format\":${imageInfo.format},\"user_id\":123456}";
+  	private String callbackBody = "{\"bucket\":${bucket},\"mimeType\":${mimeType},\"size\":${size},\"object\":${object},\"etag\":${etag},\"imageHeight\":${imageInfo.height},\"imageWidth\":${imageInfo.width},\"imageType\":${imageInfo.format},\"user_id\":123456}";
   	
   	@RequestMapping(value = "/getOssSign",method = RequestMethod.GET)
     public getOssSignResponseBodyData getOssSign(){
@@ -102,77 +116,26 @@ public class video {
 		try {
 			String ossCallbackBody = GetPostBody(request.getInputStream(), Integer.parseInt(request.getHeader("content-length")));
 			LOG.info("oss is:"+ossCallbackBody);
-//			boolean ret = VerifyOSSCallbackRequest(request, ossCallbackBody);
-//			callBackBodyData advertiseData = null;
-//			try {
-//				advertiseData = JsonUtils.fromJson(ossCallbackBody, new TypeReference<callBackBodyData>() {});
-//			} catch (JsonParseException e) {
-//				e.printStackTrace();
-//			} catch (JsonMappingException e) {
-//				e.printStackTrace();
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//			LOG.info("verify result:" + ret);
-//			LOG.info("OSS Callback content:" + ossCallbackBody);
-//			LOG.info("etag is " + advertiseData.getEtag());
-//			if (ret)
-//			{
-//				aliUpLoadCalllBackService.saveUploadFileInfo(ossCallbackBody);
-//				response(request, response, "{\"myetag\":\""+advertiseData.getEtag()+"\"}", HttpServletResponse.SC_OK,ossCallbackBody);
-////				response(request, response, "{\"myetag\":\"aaaa\"}", HttpServletResponse.SC_OK,ossCallbackBody);
-//			}
-//			else
-//			{
-//				response(request, response, "{\"Status\":\"verdify not ok\"}", HttpServletResponse.SC_BAD_REQUEST,ossCallbackBody);
-//			}
+			boolean ret = VerifyOSSCallbackRequest(request, ossCallbackBody);
+			Gson gson = new Gson();
+			callBackBodyData advertiseData = gson.fromJson(ossCallbackBody, callBackBodyData.class);
+			LOG.info("verify result:" + ret);
+			LOG.info("OSS Callback content:" + ossCallbackBody);
+			LOG.info("etag is " + advertiseData.getEtag());
+			if (ret)
+			{
+				response(request, response, "{\"myetag\":\""+advertiseData.getEtag()+"\"}", HttpServletResponse.SC_OK,ossCallbackBody);
+			}
+			else
+			{
+				response(request, response, "{\"Status\":\"verdify not ok\"}", HttpServletResponse.SC_BAD_REQUEST,ossCallbackBody);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		LOG.info("阿里上传回调结束");
 	}
-//  	protected boolean VerifyOSSCallbackRequest(HttpServletRequest request, String ossCallbackBody) throws NumberFormatException, IOException
-//	{
-//		boolean ret = false;	
-//		String autorizationInput = new String(request.getHeader("Authorization"));
-//		String pubKeyInput = request.getHeader("x-oss-pub-key-url");//公钥
-//		byte[] authorization = BinaryUtil.fromBase64String(autorizationInput);
-//		byte[] pubKey = BinaryUtil.fromBase64String(pubKeyInput);
-//		String pubKeyAddr = new String(pubKey);
-//		if (!pubKeyAddr.startsWith("http://gosspublic.alicdn.com/") && !pubKeyAddr.startsWith("https://gosspublic.alicdn.com/"))
-//		{
-//			System.out.println("pub key addr must be oss addrss");
-//			return false;
-//		}
-//		String retString = executeGet(pubKeyAddr);
-//		retString = retString.replace("-----BEGIN PUBLIC KEY-----", "");
-//		retString = retString.replace("-----END PUBLIC KEY-----", "");
-//		String queryString = request.getQueryString();
-//		String uri = request.getRequestURI();
-//		String decodeUri = java.net.URLDecoder.decode(uri, "UTF-8");
-//		String authStr = decodeUri;
-//		if (queryString != null && !queryString.equals("")) {
-//			authStr += "?" + queryString;
-//		}
-//		authStr += "\n" + ossCallbackBody;
-//		ret = doCheck(authStr, authorization, retString);
-//		return ret;
-//	}
-
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//			throws ServletException, IOException {
-//		String ossCallbackBody = GetPostBody(request.getInputStream(),
-//				Integer.parseInt(request.getHeader("content-length")));
-//		boolean ret = VerifyOSSCallbackRequest(request, ossCallbackBody);
-//		System.out.println("verify result : " + ret);
-//		// System.out.println("OSS Callback Body:" + ossCallbackBody);
-//		if (ret) {
-//			response(request, response, "{\"Status\":\"OK\"}", HttpServletResponse.SC_OK);
-//		} else {
-//			response(request, response, "{\"Status\":\"verdify not ok\"}", HttpServletResponse.SC_BAD_REQUEST);
-//		}
-//	}
-	public String GetPostBody(InputStream is, int contentLen) {
+  	public String GetPostBody(InputStream is, int contentLen) {
 		if (contentLen > 0) {
 			int readLen = 0;
 			int readLengthThisTime = 0;
@@ -191,6 +154,89 @@ public class video {
 		}
 		return "";
 	}
+	
+	protected boolean VerifyOSSCallbackRequest(HttpServletRequest request, String ossCallbackBody) throws NumberFormatException, IOException
+	{
+		boolean ret = false;	
+		String autorizationInput = new String(request.getHeader("Authorization"));
+		String pubKeyInput = request.getHeader("x-oss-pub-key-url");//公钥
+		byte[] authorization = BinaryUtil.fromBase64String(autorizationInput);
+		byte[] pubKey = BinaryUtil.fromBase64String(pubKeyInput);
+		String pubKeyAddr = new String(pubKey);
+		if (!pubKeyAddr.startsWith("http://gosspublic.alicdn.com/") && !pubKeyAddr.startsWith("https://gosspublic.alicdn.com/"))
+		{
+			System.out.println("pub key addr must be oss addrss");
+			return false;
+		}
+		String retString = executeGet(pubKeyAddr);
+		retString = retString.replace("-----BEGIN PUBLIC KEY-----", "");
+		retString = retString.replace("-----END PUBLIC KEY-----", "");
+		String queryString = request.getQueryString();
+		String uri = request.getRequestURI();
+		String decodeUri = java.net.URLDecoder.decode(uri, "UTF-8");
+		String authStr = decodeUri;
+		if (queryString != null && !queryString.equals("")) {
+			authStr += "?" + queryString;
+		}
+		authStr += "\n" + ossCallbackBody;
+		ret = doCheck(authStr, authorization, retString);
+		return ret;
+	}
+	
+	@SuppressWarnings("finally")
+	public String executeGet(String url) {
+		BufferedReader in = null;
+
+		String content = null;
+		try {
+			// 定义HttpClient
+			HttpClient client = HttpClientBuilder.create().build();
+			// 实例化HTTP方法
+			HttpGet request = new HttpGet();
+			request.setURI(new URI(url));
+			HttpResponse response = client.execute(request);
+
+			in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			StringBuffer sb = new StringBuffer("");
+			String line = "";
+			String NL = System.getProperty("line.separator");
+			while ((line = in.readLine()) != null) {
+				sb.append(line + NL);
+			}
+			in.close();
+			content = sb.toString();
+		} catch (Exception e) {
+		} finally {
+			if (in != null) {
+				try {
+					in.close();// 最后要关闭BufferedReader
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return content;
+		}
+	}
+	
+	
+	public static boolean doCheck(String content, byte[] sign, String publicKey) {
+		try {
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			byte[] encodedKey = BinaryUtil.fromBase64String(publicKey);
+			PublicKey pubKey = keyFactory.generatePublic(new X509EncodedKeySpec(encodedKey));
+			java.security.Signature signature = java.security.Signature.getInstance("MD5withRSA");
+			signature.initVerify(pubKey);
+			signature.update(content.getBytes());
+			boolean bverify = signature.verify(sign);
+			return bverify;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
 	private void response(HttpServletRequest request, HttpServletResponse response, String results, int status,String ossCallbackBody) throws IOException {
 		String callbackFunName = request.getParameter("callback");
 		LOG.info("callbackFunName is:"+callbackFunName);
--- video/video-web/src/main/java/com/performer/player/video/entity/callBackBodyData.java
+++ video/video-web/src/main/java/com/performer/player/video/entity/callBackBodyData.java
@@ -1,9 +1,94 @@
 package com.performer.player.video.entity;
 
 public class callBackBodyData {
+	/**
+	 * oss存储空间
+	 */
 	public String bucket;
+	/**
+	 * 存储文件类型
+	 */
 	public String mimeType;
+	/**
+	 * 文件大小
+	 */
 	public String size;
+	/**
+	 * 存放路径 url
+	 */
 	public String object;
+	/**
+	 * 单个文件的md5码（分段上传的文件每段的md5都不同）
+	 */
 	public String etag;
+	/**
+	 * 图片长 上传是图片的情况下
+	 */
+	public String imageHeight;
+	/**
+	 * 图片宽
+	 */
+	public String imageWidth;
+	/**
+	 * 图片类型
+	 */
+	public String imageType;
+	/**
+	 * 自定义参数 用户id
+	 */
+	public String user_id;
+	public String getBucket() {
+		return bucket;
+	}
+	public void setBucket(String bucket) {
+		this.bucket = bucket;
+	}
+	public String getMimeType() {
+		return mimeType;
+	}
+	public void setMimeType(String mimeType) {
+		this.mimeType = mimeType;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getObject() {
+		return object;
+	}
+	public void setObject(String object) {
+		this.object = object;
+	}
+	public String getEtag() {
+		return etag;
+	}
+	public void setEtag(String etag) {
+		this.etag = etag;
+	}
+	public String getImageHeight() {
+		return imageHeight;
+	}
+	public void setImageHeight(String imageHeight) {
+		this.imageHeight = imageHeight;
+	}
+	public String getImageWidth() {
+		return imageWidth;
+	}
+	public void setImageWidth(String imageWidth) {
+		this.imageWidth = imageWidth;
+	}
+	public String getImageType() {
+		return imageType;
+	}
+	public void setImageType(String imageType) {
+		this.imageType = imageType;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
 }
