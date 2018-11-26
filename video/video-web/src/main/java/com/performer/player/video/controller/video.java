ae3152a49d237bed0a204f01b21805b378770ed5
lin1heart <lin1heart@gmail.com>
2018-11-26 21:42:30 +0800

    fix

--- video/video-web/src/main/java/com/performer/player/video/controller/video.java
+++ video/video-web/src/main/java/com/performer/player/video/controller/video.java
@@ -6,17 +6,27 @@ import com.aliyun.oss.model.PolicyConditions;
 import com.google.gson.Gson;
 import com.performer.player.video.entity.callBackBodyData;
 import com.performer.player.video.entity.getOssSignResponseBodyData;
-import com.performer.player.video.util.uploadMethod;
 
 import net.sf.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -105,9 +115,9 @@ public class video {
   	@RequestMapping(value = "/callback",method = RequestMethod.POST)
   	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String ossCallbackBody = uploadMethod.GetPostBody(request.getInputStream(),
+		String ossCallbackBody = GetPostBody(request.getInputStream(),
 				Integer.parseInt(request.getHeader("content-length")));
-		boolean ret = uploadMethod.VerifyOSSCallbackRequest(request, ossCallbackBody);
+		boolean ret = VerifyOSSCallbackRequest(request, ossCallbackBody);
 		System.out.println("verify result : " + ret);
 		LOG.info("OSS Callback Body:" + ossCallbackBody);
 		Gson gson = new Gson();
@@ -116,9 +126,159 @@ public class video {
 		LOG.info(""+id);
 		
 		if (ret) {
-			uploadMethod.response(request, response, "{\"Status\":\"OK\"}", HttpServletResponse.SC_OK);
+			response(request, response, "{\"Status\":\"OK\"}", HttpServletResponse.SC_OK);
 		} else {
-			uploadMethod.response(request, response, "{\"Status\":\"verdify not ok\"}", HttpServletResponse.SC_BAD_REQUEST);
+			response(request, response, "{\"Status\":\"verdify not ok\"}", HttpServletResponse.SC_BAD_REQUEST);
 		}
+	}
+  	
+  	/**
+	 * 获取public key
+	 * 
+	 * @param url
+	 * @return
+	 */
+	@SuppressWarnings("finally")
+	public String executeGet(String url) {
+		BufferedReader in = null;
+		String content = null;
+		try {
+			// 定义HttpClient
+			HttpClient client = HttpClientBuilder.create().build();
+			// 实例化HTTP方法
+			HttpGet request = new HttpGet();
+			request.setURI(new URI(url));
+			HttpResponse response = client.execute(request);
+			in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			StringBuffer sb = new StringBuffer("");
+			String line = "";
+			String NL = System.getProperty("line.separator");
+			while ((line = in.readLine()) != null) {
+				sb.append(line + NL);
+			}
+			in.close();
+			content = sb.toString();
+		} catch (Exception e) {
+		} finally {
+			if (in != null) {
+				try {
+					in.close();// 最后要关闭BufferedReader
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return content;
+		}
+	}
+
+	/**
+	 * 获取Post消息体
+	 * 
+	 * @param is
+	 * @param contentLen
+	 * @return
+	 */
+	public String GetPostBody(InputStream is, int contentLen) {
+		if (contentLen > 0) {
+			int readLen = 0;
+			int readLengthThisTime = 0;
+			byte[] message = new byte[contentLen];
+			try {
+				while (readLen != contentLen) {
+					readLengthThisTime = is.read(message, readLen, contentLen - readLen);
+					if (readLengthThisTime == -1) {// Should not happen.
+						break;
+					}
+					readLen += readLengthThisTime;
+				}
+				return new String(message);
+			} catch (IOException e) {
+			}
+		}
+		return "";
+	}
+
+	/**
+	 * 验证上传回调的Request
+	 * 
+	 * @param request
+	 * @param ossCallbackBody
+	 * @return
+	 * @throws NumberFormatException
+	 * @throws IOException
+	 */
+	public boolean VerifyOSSCallbackRequest(HttpServletRequest request, String ossCallbackBody)
+			throws NumberFormatException, IOException {
+		boolean ret = false;
+		String autorizationInput = new String(request.getHeader("Authorization"));
+		String pubKeyInput = request.getHeader("x-oss-pub-key-url");
+		byte[] authorization = BinaryUtil.fromBase64String(autorizationInput);
+		byte[] pubKey = BinaryUtil.fromBase64String(pubKeyInput);
+		String pubKeyAddr = new String(pubKey);
+		if (!pubKeyAddr.startsWith("http://gosspublic.alicdn.com/")
+				&& !pubKeyAddr.startsWith("https://gosspublic.alicdn.com/")) {
+			System.out.println("pub key addr must be oss addrss");
+			return false;
+		}
+		String retString = executeGet(pubKeyAddr);
+		retString = retString.replace("-----BEGIN PUBLIC KEY-----", "");
+		retString = retString.replace("-----END PUBLIC KEY-----", "");
+		String queryString = request.getQueryString();
+		String uri = request.getRequestURI();
+		String decodeUri = java.net.URLDecoder.decode(uri, "UTF-8");
+		String authStr = decodeUri;
+		if (queryString != null && !queryString.equals("")) {
+			authStr += "?" + queryString;
+		}
+		authStr += "\n" + ossCallbackBody;
+		ret = doCheck(authStr, authorization, retString);
+		return ret;
+	}
+
+	/**
+	 * 验证RSA
+	 * 
+	 * @param content
+	 * @param sign
+	 * @param publicKey
+	 * @return
+	 */
+	public boolean doCheck(String content, byte[] sign, String publicKey) {
+		try {
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			byte[] encodedKey = BinaryUtil.fromBase64String(publicKey);
+			PublicKey pubKey = keyFactory.generatePublic(new X509EncodedKeySpec(encodedKey));
+			java.security.Signature signature = java.security.Signature.getInstance("MD5withRSA");
+			signature.initVerify(pubKey);
+			signature.update(content.getBytes());
+			boolean bverify = signature.verify(sign);
+			return bverify;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	/**
+	 * 服务器响应结果
+	 * 
+	 * @param request
+	 * @param response
+	 * @param results
+	 * @param status
+	 * @throws IOException
+	 */
+	public void response(HttpServletRequest request, HttpServletResponse response, String results, int status)
+			throws IOException {
+		String callbackFunName = request.getParameter("callback");
+		response.addHeader("Content-Length", String.valueOf(results.length()));
+		if (callbackFunName == null || callbackFunName.equalsIgnoreCase(""))
+			response.getWriter().println(results);
+		else
+			response.getWriter().println(callbackFunName + "( " + results + " )");
+		response.setStatus(status);
+		response.flushBuffer();
 	}
 }
