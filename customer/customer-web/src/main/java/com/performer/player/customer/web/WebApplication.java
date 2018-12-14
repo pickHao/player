8f6b02edccfb01e00316623c45ea82ae3021485a
PickHao <zzh10win@163.com>
2018-12-14 13:08:16 +0800

    用户登录

--- customer/customer-web/src/main/java/com/performer/player/customer/web/WebApplication.java
+++ customer/customer-web/src/main/java/com/performer/player/customer/web/WebApplication.java
@@ -3,7 +3,6 @@ package com.performer.player.customer.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"com.performer.player.customer"})
