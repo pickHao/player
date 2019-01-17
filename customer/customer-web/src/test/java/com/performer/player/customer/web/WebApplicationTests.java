f4bf42eb527679877927c87508aeb37fb7ac3af0
Administrator <zzh10win163.com>
2019-01-17 18:06:23 +0800

    redis配置

--- customer/customer-web/src/test/java/com/performer/player/customer/web/WebApplicationTests.java
+++ customer/customer-web/src/test/java/com/performer/player/customer/web/WebApplicationTests.java
@@ -2,15 +2,48 @@ package com.performer.player.customer.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebApplicationTests {
 
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    Redisson redisson;
+
+    @Resource
+    RedissonClient redissonClient;
+
     @Test
     public void contextLoads() {
+
+        stringRedisTemplate.opsForValue().set("append","asss");
+        String append = stringRedisTemplate.opsForValue().get("append");
+        System.out.println("!!!!"+append);
+
     }
+
+    @Test
+    public void test01(){
+
+        Map map = redissonClient.getMap("");
+
+
+    }
+
+
+
+
 
 }
